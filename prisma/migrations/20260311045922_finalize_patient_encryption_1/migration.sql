ad7a34762d0e31e8a11be9ed7dd05ecd69df4a44
zoe <jsh010322@gmail.com>
2026-03-11 13:59:27 +0900

    amend

--- prisma/migrations/20260311045922_finalize_patient_encryption_1/migration.sql
+++ prisma/migrations/20260311045922_finalize_patient_encryption_1/migration.sql
@@ -4,7 +4,6 @@
   - You are about to drop the column `date_of_birth` on the `patient` table. All the data in the column will be lost.
   - You are about to drop the column `registration_number` on the `patient` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[registration_number_hash,hospital_id]` on the table `patient` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `registration_number_hash` to the `patient` table without a default value. This is not possible if the table is not empty.
   - Made the column `encrypted_date_of_birth` on table `patient` required. This step will fail if there are existing NULL values in that column.
   - Made the column `encrypted_registration_number` on table `patient` required. This step will fail if there are existing NULL values in that column.
 
@@ -12,7 +11,7 @@
 -- AlterTable
 ALTER TABLE "patient" DROP COLUMN "date_of_birth",
 DROP COLUMN "registration_number",
-ADD COLUMN     "registration_number_hash" TEXT NOT NULL,
+ADD COLUMN     "registration_number_hash" TEXT,
 ALTER COLUMN "encrypted_date_of_birth" SET NOT NULL,
 ALTER COLUMN "encrypted_registration_number" SET NOT NULL;
 
